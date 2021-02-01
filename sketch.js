f00b4f60babeabea2705efd2469eeeed40113181
Lieuwe Schikhof <111131@st-maartenscollege.nl>
2021-02-01 14:54:27 +0000

    mapje image toegevoegd

--- sketch.js
+++ sketch.js
@@ -18,7 +18,23 @@ function startScreen(){
 		background(96, 157, 255)
 		fill(255)
 		textAlign(CENTER);
-		text('WELCOME TO MY CATCHING GAME', width / 2, height / 2)
+		text('WELCOME TO RIOTWARS', width / 2, height / 2)
 		text('click to start', width / 2, height / 2 + 20);
-		reset();
+
+}
+
+function gameOn(){
+		background(0, 0, 0)
+		fill(255)
+		textAlign(CENTER);
+		text('Hier is t spel', width / 2, height / 2)
+		text('click to start', width / 2, height / 2 + 20);
+}
+
+
+
+function mousePressed(){
+	if(screen==0){
+  	screen=1
+  }
 }
