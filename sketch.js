4e0adaaecf631be30ccb3c7e92e99591ff6f2f2d
Wouter Paardekooper <110270@st-maartenscollege.nl>
2021-02-04 13:43:59 +0000

    De achtergrond van het menu is een plaatje en er zijn twee knoppen in het menu die van kleur veranderen als de muis eroverheen gaat.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 var screen = 0;
+let bg;
+let oranje;
 
 function setup() {
+  oranje = color(246, 178, 107);
+  bg = loadImage('image/BackgroundMenu.png');
   createCanvas(600, 400);
 }
 
@@ -15,12 +19,31 @@ function draw() {
 }
 
 function startScreen(){
-		background(96, 157, 255)
-		fill(255)
-		textAlign(CENTER);
-		text('WELCOME TO RIOTWARS', width / 2, height / 2)
-		text('click to start', width / 2, height / 2 + 20);
+    background(bg); //zorgt voor achtergrond plaatje
 
+    fill(oranje);
+    noStroke();
+    if ((mouseX > width/2-100) && (mouseX < width/2+100) && (mouseY > height/2-60) && (mouseY < height/2-10)){
+      fill(150);
+    }
+    else{
+      fill(oranje)
+    }
+    rect(width/2 -100,height/2 - 60,200,50,10)
+    
+    if ((mouseX > width/2-100) && (mouseX < width/2+100) && (mouseY > height/2) && (mouseY < height/2+50)){
+      fill(150);
+    }
+    else{
+      fill(oranje)
+    }
+    rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+
+    textAlign(CENTER)
+    textSize(25)
+    fill(0)
+		text('HIGHSCORES', width / 2, height / 2 + 35);
+		text('START', width/2,height/2 -25)
 }
 
 function gameOn(){
