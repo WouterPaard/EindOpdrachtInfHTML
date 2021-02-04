de8df24ff36796dede14951d9cb0ac99d2b7cffe
Wouter Paardekooper <110270@st-maartenscollege.nl>
2021-02-04 16:49:30 +0000

    De knoppen in het menu sturen je nu naar de goede schermen. en de knoppen veranderen van kleur als je er overheen gaat

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 var screen = 0;
 let bg;
+let logo;
 let oranje;
 
 function setup() {
   oranje = color(246, 178, 107);
   bg = loadImage('image/BackgroundMenu.png');
+  logo = loadImage('image/logo.png')
   createCanvas(600, 400);
 }
 
@@ -16,28 +18,55 @@ function draw() {
   }else if(screen==2){
   	endScreen()
   }
+  else if(screen==3){
+    highScoreScreen()
+  }
 }
 
 function startScreen(){
     background(bg); //zorgt voor achtergrond plaatje
-
-    fill(oranje);
+    image(logo, width/2 - 75, 15, 150, 120)
     noStroke();
     if ((mouseX > width/2-100) && (mouseX < width/2+100) && (mouseY > height/2-60) && (mouseY < height/2-10)){
-      fill(150);
+      fill(223, 130, 22);
+      rect(width/2 -100,height/2 - 60,200,50,10)
+      cursor("pointer")
+      fill(oranje)
+      rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+      if(mouseIsPressed){
+        screen = 1;
+      }
+
+    }
+    else if((mouseX > width/2-100) && (mouseX < width/2+100) && (mouseY > height/2) && (mouseY < height/2+50)){
+      fill(223, 130, 22);
+      rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+      fill(oranje)
+      rect(width/2 -100,height/2 - 60,200,50,10)     
+      cursor("pointer")
+      if(mouseIsPressed){
+        screen = 3;
+      }
     }
     else{
       fill(oranje)
+      rect(width/2 -100,height/2 - 60,200,50,10)
+      rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+      cursor()
     }
-    rect(width/2 -100,height/2 - 60,200,50,10)
-    
+
+    /*
     if ((mouseX > width/2-100) && (mouseX < width/2+100) && (mouseY > height/2) && (mouseY < height/2+50)){
-      fill(150);
+      fill(223, 130, 22);
+    rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+      cursor("pointer")
     }
     else{
       fill(oranje)
-    }
     rect(width/2 -100,height/2,200,50,10) //zorgt voor die oranje knoppen
+      cursor()
+    }
+*/
 
     textAlign(CENTER)
     textSize(25)
@@ -54,10 +83,15 @@ function gameOn(){
 		text('click to start', width / 2, height / 2 + 20);
 }
 
+function highScoreScreen(){
+  background(0, 0, 0)
+		fill(255)
+		textAlign(CENTER);
+		text('scoreboard', width / 2, height / 2)
+		text('komt hierzo', width / 2, height / 2 + 20);
+}
 
 
 function mousePressed(){
-	if(screen==0){
-  	screen=1
-  }
+
 }
